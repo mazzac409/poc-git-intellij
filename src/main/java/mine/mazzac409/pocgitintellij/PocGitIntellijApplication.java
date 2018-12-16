784c0eafb72d02fc422d55f3c6d4dfb1ebf23c20
mazzac409 <mazzachris.work@gmail.com>
2018-12-16 11:52:54 -0600

    Initial commit

---
+++ src/main/java/mine/mazzac409/pocgitintellij/PocGitIntellijApplication.java
@@ -0,0 +1,14 @@
+package mine.mazzac409.pocgitintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocGitIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocGitIntellijApplication.class, args);
+	}
+
+}
+
